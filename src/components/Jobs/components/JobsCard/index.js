7bfa8a7c3318468f59a768aa22982c2143bd71db
Archaengel <god11341258@gmail.com>
2020-04-06 17:14:52 -0700

    Remove divider in job card

--- src/components/Jobs/components/JobsCard/index.js
+++ src/components/Jobs/components/JobsCard/index.js
@@ -2,14 +2,15 @@ import React from 'react';
 import moment from 'moment';
 import { Card, List, Tag, Typography } from 'antd';
 
-const { Paragraph, Text } = Typography;
+const { Paragraph, Text, Title } = Typography;
 
 export const JobCard = ({ job }) => {
   const { title, description, postedOn, skills } = job;
   const datePosted = new Date(postedOn);
   return (
     <List.Item>
-      <Card title={title} hoverable>
+      <Card hoverable className="job-card__card">
+        <Title level={3}>{title}</Title>
         <Paragraph>{description}</Paragraph>
         <br />
         <List
