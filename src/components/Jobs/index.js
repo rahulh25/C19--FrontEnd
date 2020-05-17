a6b369caed2d9ba06f05e48cff043fc14ee8f3f7
Harish Sivaprakash <ksharish72@gmail.com>
2020-05-17 12:50:36 -0400

    Toolbar added, jobs displayed with pagination

--- src/components/Jobs/index.js
+++ src/components/Jobs/index.js
@@ -1,31 +1,35 @@
-import React, { useState, useLayoutEffect } from 'react';
-import { ErrorBanner } from '../../lib/components';
-import List from '@material-ui/core/List';
-import Container from '@material-ui/core/Container';
-import Pagination from '@material-ui/lab/Pagination';
-import { JobsHeader, JobCard, JobsSkeleton } from './components';
-import { usePaginatedQuery } from 'react-query'
-import { getPaginatedJobsBySearch } from '../../lib/api'
-import { makeStyles } from '@material-ui/core/styles';
-import './styles/index.css';
+import React, { useState, useLayoutEffect } from "react";
+import { ErrorBanner } from "../../lib/components";
+import List from "@material-ui/core/List";
+import Container from "@material-ui/core/Container";
+import Pagination from "@material-ui/lab/Pagination";
+import { JobsHeader, JobCard, JobsSkeleton } from "./components";
+import { usePaginatedQuery } from "react-query";
+import { getPaginatedJobsBySearch } from "../../lib/api";
+import { makeStyles } from "@material-ui/core/styles";
+import "./styles/index.css";
+import { Toolbar } from "../Toolbar";
 
 const PAGE_SIZE = 5;
 const useStyles = makeStyles({
   pagination: {
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   container: {
-    minHeight: '100%',
-    width: '100%',
-    padding: '16px',
+    minHeight: "100%",
+    width: "100%",
+    padding: "16px",
   },
 });
 
 export const Jobs = () => {
   const classes = useStyles();
   const [currentPage, setCurrentPage] = useState(1);
-  const [query, setQuery] = useState('');
-  const { resolvedData, isFetching, status, error } = usePaginatedQuery(['jobs', query, currentPage, PAGE_SIZE], getPaginatedJobsBySearch)
+  const [query, setQuery] = useState("");
+  const { resolvedData, isFetching, status, error } = usePaginatedQuery(
+    ["jobs", query, currentPage, PAGE_SIZE],
+    getPaginatedJobsBySearch
+  );
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
@@ -37,42 +41,46 @@ export const Jobs = () => {
   };
 
   if (status === "loading") {
-    return (<Container classes={{ root: classes.container }}>
-      <JobsSkeleton />
-    </Container>
+    return (
+      <Container classes={{ root: classes.container }}>
+        <JobsSkeleton />
+      </Container>
     );
   }
 
   if (status === "error") {
-    return (<Container classes={{ root: classes.container }}>
-      <ErrorBanner error={error} message={error} />
-      <JobsSkeleton />
-    </Container>
+    return (
+      <Container classes={{ root: classes.container }}>
+        <ErrorBanner error={error} message={error} />
+        <JobsSkeleton />
+      </Container>
     );
   }
 
-  const jobsElement = resolvedData && resolvedData.result && resolvedData.total ? (
-    <>
-      <List subheader={<JobsHeader setQuery={setQuery} />}>
-        {isFetching && <JobsSkeleton />}
-        {!isFetching &&
-          resolvedData.result &&
-          resolvedData.result.map((item) => <JobCard key={item._id} job={item} />)}
-      </List>
-      <Pagination
-        count={
-          Math.floor(resolvedData.total / PAGE_SIZE) +
-          (resolvedData.total % PAGE_SIZE === 0 ? 0 : 1)
-        }
-        page={currentPage}
-        onChange={handlePageChange}
-        classes={{ ul: classes.pagination }}
-      />
-    </>
-  ) : null
+  const jobsElement =
+    resolvedData && resolvedData.length ? (
+      <>
+        <List subheader={<JobsHeader setQuery={setQuery} />}>
+          {isFetching && <JobsSkeleton />}
+          {!isFetching &&
+            resolvedData &&
+            resolvedData.map((item) => <JobCard key={item._id} job={item} />)}
+        </List>
+        <Pagination
+          count={
+            Math.floor(resolvedData.length / PAGE_SIZE) +
+            (resolvedData.length % PAGE_SIZE === 0 ? 0 : 1)
+          }
+          page={currentPage}
+          onChange={handlePageChange}
+          classes={{ ul: classes.pagination }}
+        />
+      </>
+    ) : null;
 
   return (
     <Container classes={{ root: classes.container }}>
+      <Toolbar />
       {jobsElement}
     </Container>
   );
