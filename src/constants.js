30d59978606dfd05edfa7e342ad07dcf467a24ad
unknown <handoo.r@northeastern.edu>
2020-05-22 17:51:33 -0400

    Adding kubernetes deployment

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 //export const API_URL = "http://localhost:3000/";
-export const API_URL=process.env.REACT_APP_API_URL
+export const API_URL="http://a64ab8a727aa9447baecb857aca27bec-1035675610.us-east-1.elb.amazonaws.com:3000/"
 export const ADD_USER_API = `${API_URL}dev/user/registration/`;
 export const GET_USERS = `${API_URL}dev/user/`;
 export const SIGNIN_USER_API = `${API_URL}dev/user/login`;
